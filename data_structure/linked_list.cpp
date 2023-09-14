3dea6566b734bafaf4ed8a725200f8f65f0f551f
MaRoco <luigi1215@naver.com>
2023-09-14 17:25:20 +0900

    docs(add): DS linked list

--- data_structure/linked_list.cpp
+++ data_structure/linked_list.cpp
@@ -29,4 +29,65 @@ linked list
 단점
 - link field 만큼의 추가 메모리 공간 사용
 - 순서대로 data를 탐색해 내려가야함 <-> Array는 random access가 가능
+
+1. Traversal: 자료를 순차적으로 보는 것
+- array: 반복문
+    <PSEUDO>
+    for(int i=0; i<=n; i++) print data[i];
+- linked list: 메모리의 주소를 직접 컨트롤
+    1) head node의 data 출력
+    2) head node의 link field 읽음
+    3) 2)에 있던 주소로 가서 next node의 data 출력
+    ...
+    convention: ITEM(p): node의 data filed, LINK(p): node의 link filed
+    p: aux, ptr variable, 현재 들여다 볼 node를 가리키는 변수
+    <PSEUDO>
+    p<-l    // head node
+    while(p!=NULL){
+        print ITEM(p)
+        P<-link(P)
+    }
+
+2. Insertion and Deletion
+    convention: new(), free(p)  // new: node 하나를 만들 수 있는 공간 할당 함수, 주소 return -> 메모리 공간 없을 시 NULL return
+                                // free: node 삭제 시, 공간 반환 함수
+
+    1) Insertion: p: 자식 노드의 주소, q: new()값을 받을 변수
+    <PSEUDO>
+    q<-new()
+    if(q==NULL) FAILURE
+    LINK(q)<-LINK(p)
+    LINK(p)<-q
+
+    2) Deletion: p: 자식 노드의 주소, q: 삭제할 노드의 주소
+    <PSEUDO>
+    LINK(p)<-LINK(q)
+    free(q)
+
+Stack using a linked list
+generic: push, pop 사용 시 overflow 발생 x -> 평상시, 자주 발생
+exceptional: overflow가 발생 할 수 있는 상황
+우선순위: generic >>> exceptional
+linked list -> head 위치는 알고 있음 -> head를 top으로 생각
+    <PSEUDO>
+    top{
+        init: top<-NULL
+    }
+    push(x){
+        p<-new()
+        // full: OVERFLOW, 자기 자신으로 돌아옴 -> new()가 NULL ptr return
+        if(p==NULL) OVERFLOW
+        LINK(p)<-top
+        top<-p
+    }
+    pop(){
+        // empty: UNDERFLOW, 자기 자신으로 돌아옴 -> LINK(p)가 NULL
+        if(top==NULL) UNDERFLOW
+        p<-top
+        y<-ITEM(p)
+        top<-LINK(p)
+        free(p)
+
+        return y
+    }
 */
