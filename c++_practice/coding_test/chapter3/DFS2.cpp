1a8d13794c0372f4f04d625a644539c7fe63acf2
MaRoco <luigi1215@naver.com>
2023-09-14 15:08:23 +0900

    docs: ct_ch4 DFS2

--- c++_practice/coding_test/chapter3/DFS2.cpp
+++ c++_practice/coding_test/chapter3/DFS2.cpp
@@ -12,39 +12,46 @@ output: A,B,C,D,E가 존재하면 1, 아니면 0
 #include<iostream>
 #include<vector>
 
-std::vector<int> friends[2000];
-bool visited[2000];
-
-void check_five_friends(int x, int cnt);
+std::vector<int> nodes[2000];
+std::vector<bool> visit(2000);
+bool arrive;
+void DFS(int node, int depth);
 
 int main(){
     std::ios::sync_with_stdio(false);
     std::cin.tie(NULL);
     std::cout.tie(NULL);
-    int n, m, a, b;
+    int n,m,a,b;
     std::cin>>n>>m;
-    int cnt = 0;
+    nodes->resize(n);
+    visit = std::vector<bool>(n,false);
+    arrive = false;
     for(int i=0; i<m; i++){
         std::cin>>a>>b;
-        friends[a].push_back(b);
+        nodes[a].push_back(b);
+        nodes[b].push_back(a);
     }
-    check_five_friends(1, cnt);
-    if(cnt == 5)    std::cout<<1<<'\n';
-    else            std::cout<<0<<'\n';
+    for(int i=0; i<n; i++)
+        DFS(i,1);
+    if(arrive)
+        std::cout<<1<<'\n';
+    else
+        std::cout<<0<<'\n';
+    return 0;
 }
-/* ex) 01, 12, 23, 34 input
-    friends[0] = {1}
-    friends[1] = {2}
-    friends[2] = {3}
-    friends[3] = {4}
-*/
-// friends에 대한 그래프 탐색
-void check_five_friends(int x, int cnt){
-    visited[x] = true;
-    cnt++;
-    for(int i=0; i<friends[x].size(); i++){
-        int y = friends[x][i];
-        if(!visited[y])
-            check_five_friends(y, cnt);
+
+// 그래프 탐색
+void DFS(int node, int depth){
+    if(depth==5||arrive){
+        arrive=true;
+        return;
     }
+    visit[node] = true;
+    for(int i=0; i<nodes[node].size(); i++){
+        int next_node=nodes[node][i];
+        if(!visit[next_node]){
+            DFS(next_node, depth+1);
+        }
+    }
+    visit[node] = false;
 }
