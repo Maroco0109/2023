41872282a7c128c1f3030c38b41535d93f260ca1
MaRoco <luigi1215@naver.com>
2023-10-30 13:02:26 +0900

    docs(fix) : Helloworld 수정
    자바 컴파일 과정 주석 추가

--- java/Helloworld.java
+++ java/Helloworld.java
@@ -1,3 +1,5 @@
+// source == code == language   -->   application == program
+// java source code(.java) -compile->  java app(.class) -run->  java virtual machine    -run->  computer
 public class Helloworld{
     public static void main(String[] args){
         System.out.println("Hello World!");
